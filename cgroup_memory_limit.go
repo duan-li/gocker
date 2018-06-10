5453ce3abfae1aa411bb3435c5ebf40f5031c3d6
inputx <inputx@goodays.com>
2018-06-10 12:18:00 +0000

    add comment into demo file

--- cgroup_memory_limit.go
+++ cgroup_memory_limit.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	// "io/ioutil"
 	"os"
 	"os/exec"
-	"path"
-	"strconv"
+	// "path"
+	// "strconv"
 	"syscall"
 )
 
 const cgroupMemoryHierarchyMount = "/sys/fs/cgroup/memory"
 const memoryLimit = "100m"
+const execCommand = "/proc/self/exe"
 
 func main() {
-	if os.Args[0] == "/proc/self/exe" {
+	// all about container process
+	if os.Args[0] == execCommand {
 		fmt.Printf("current pid %d", syscall.Getpid())
 		fmt.Println()
 		cmd := exec.Command("sh", "-c", `stress --vm-bytes 200m --vm-keep -m 1`)
@@ -28,10 +30,14 @@ func main() {
 		}
 	}
 
-	cmd := exec.Command("/proc/self/exe")
+	cmd := exec.Command(execCommand)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
+		// UTS Namespace
+		// PID Namespace
+		// Mount Namespace
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -39,9 +45,13 @@ func main() {
 		fmt.Println("ERROR", err)
 		os.Exit(1)
 	} else {
-		fmt.Printf("%v", cmd.Process.Pid)
+		fmt.Printf("%v ", cmd.Process.Pid)
+
+		// create cgroup on memory subsystem's Hierarchy
 		os.Mkdir(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit"), 0755)
+		// add container into this cgroup
 		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "tasks"), []byte(strconv.Itoa(cmd.Process.Pid)), 0644)
+		// limit memory for this cgroup
 		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "memory.limit_in_bytes"), []byte(memoryLimit), 0644)
 	}
 	cmd.Process.Wait()
