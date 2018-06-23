a6c4cf2b208472fd57076cbc16a003eed9dbf8e6
inputx <inputx@goodays.com>
2018-06-23 11:31:30 +0000

    add memory cgroup

---
+++ cgroups/subsystems/memory_test.go
@@ -0,0 +1,39 @@
+package subsystems
+
+import (
+	assert "github.com/stretchr/testify/assert"
+	"os"
+	"path"
+	"testing"
+)
+
+func TestMemoryName(t *testing.T) {
+	memSubSys := MemorySubSystem{}
+	assert.Equal(t, memSubSys.Name(), "memory", "Name is woring")
+}
+
+// func TestMemoryCgroup(t *testing.T) {
+// 	memSubSys := MemorySubSystem{}
+// 	resConfig := ResourceConfig{
+// 		MemoryLimit: "1000m",
+// 	}
+// 	testCgroup := "testmemlimit"
+
+// 	if err := memSubSys.Set(testCgroup, &resConfig); err != nil {
+// 		t.Fatalf("cgroup fail %v", err)
+// 	}
+// 	stat, _ := os.Stat(path.Join(FindCgroupMountpoint("memory"), testCgroup))
+// 	t.Logf("cgroup stats: %+v", stat)
+
+// 	if err := memSubSys.Apply(testCgroup, os.Getpid()); err != nil {
+// 		t.Fatalf("cgroup Apply %v", err)
+// 	}
+// 	//move process to root of Cgroup node
+// 	if err := memSubSys.Apply("", os.Getpid()); err != nil {
+// 		t.Fatalf("cgroup Apply %v", err)
+// 	}
+
+// 	if err := memSubSys.Remove(testCgroup); err != nil {
+// 		t.Fatalf("cgroup remove %v", err)
+// 	}
+// }
