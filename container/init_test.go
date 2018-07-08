2fa4d69b1a10e0e7651e258de42304c5438a2c4a
inputx <inputx@goodays.com>
2018-07-08 22:41:41 +1000

    clean up code

--- container/init_test.go
+++ container/init_test.go
@@ -13,13 +13,6 @@ func TestRunContainerInitProcess(t *testing.T) {
 	assert.Nil(t, init, "should be nil")
 }
 
-//func TestPivotRoot(t *testing.T) {
-//	pwd, err := os.Getwd()
-//	assert.Nil(t, err, "err should be nil")
-//	error := pivotRoot(pwd)
-//	assert.NotNil(t, error, "error should be nil")
-//}
-
 func TestReadUserCommand(t *testing.T) {
 	message := readUserCommand()
 	assert.NotNil(t, message, "error should be nil")
